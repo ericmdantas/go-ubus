d8e89fd57d4b46d2602394b65cf2d72da64123d7
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-12-12 08:48:04 -0200

    .

--- ubus_test.go
+++ ubus_test.go
@@ -2,6 +2,88 @@ package ubus
 
 import "testing"
 
+func BenchmarkOn(b *testing.B) {
+	b.Run("simple", func(b *testing.B) {
+		u := NewBus()
+
+		for i := 0; i < b.N; i++ {
+			u.On("a", func(_ interface{}) {})
+		}
+	})
+
+	b.Run("simple_with_emit", func(b *testing.B) {
+		u := NewBus()
+
+		for i := 0; i < b.N; i++ {
+			u.On("a", func(_ interface{}) {})
+			u.Emit("a", "x")
+		}
+	})
+}
+
+func BenchmarkOnce(b *testing.B) {
+	b.Run("simple", func(b *testing.B) {
+		u := NewBus()
+
+		for i := 0; i < b.N; i++ {
+			u.Once("a", func(_ interface{}) {})
+		}
+	})
+
+	b.Run("simple_with_emit", func(b *testing.B) {
+		u := NewBus()
+
+		for i := 0; i < b.N; i++ {
+			u.Once("a", func(_ interface{}) {})
+			u.Emit("a", "x")
+		}
+	})
+}
+
+func BenchmarkEmit(b *testing.B) {
+	b.Run("simple", func(b *testing.B) {
+		u := NewBus()
+
+		u.On("a", func(_ interface{}) {})
+
+		for i := 0; i < b.N; i++ {
+			u.Emit("a", "x")
+		}
+	})
+}
+
+func BenchmarkOff(b *testing.B) {
+	b.Run("simple", func(b *testing.B) {
+		u := NewBus()
+
+		u.On("a", func(_ interface{}) {})
+
+		for i := 0; i < b.N; i++ {
+			u.Off([]string{"a"})
+		}
+	})
+
+	b.Run("multiple", func(b *testing.B) {
+		u := NewBus()
+
+		for i := 0; i < b.N; i++ {
+			u.On("a", func(_ interface{}) {})
+			u.Off([]string{"a"})
+		}
+	})
+}
+
+func BenchmarkDestroyFn(b *testing.B) {
+	b.Run("simple", func(b *testing.B) {
+		u := NewBus()
+
+		for i := 0; i < b.N; i++ {
+			destroy := u.On("a", func(_ interface{}) {})
+			destroy()
+		}
+	})
+}
+
 func TestOn(t *testing.T) {
 	t.Run("simple", func(t *testing.T) {
 		u := NewBus()
@@ -370,6 +452,105 @@ func TestOff(t *testing.T) {
 
 		u.Emit("c", nil)
 		u.Off([]string{"c"})
+
+		if aCalled != expectedACalled {
+			t.Errorf("Expected a to have been called, but it wasn't. %t != %t", aCalled, expectedACalled)
+		}
+
+		if aCalledTimes != expectedACalledTimes {
+			t.Errorf("Expected a to have been called %d times, but it was called %d.", expectedACalledTimes, aCalledTimes)
+		}
+
+		if bCalled != expectedBCalled {
+			t.Errorf("Expected b to have been called, but it wasn't. %t != %t", bCalled, expectedBCalled)
+		}
+
+		if bCalledTimes != expectedBCalledTimes {
+			t.Errorf("Expected b to have been called %d times, but it was called %d.", expectedBCalledTimes, bCalledTimes)
+		}
+
+		if cCalled != expectedCCalled {
+			t.Errorf("Expected c to have been called, but it wasn't. %t != %t", bCalled, expectedCCalled)
+		}
+
+		if cCalledTimes != expectedCCalledTimes {
+			t.Errorf("Expected c to have been called %d times, but it was called %d.", expectedCCalledTimes, cCalledTimes)
+		}
+	})
+}
+
+func TestDestroyFn(t *testing.T) {
+	t.Run("simple", func(t *testing.T) {
+		u := NewBus()
+
+		aCalled := false
+		aCalledTimes := 0
+		expectedACalled := true
+		expectedACalledTimes := 1
+
+		destroyA := u.On("a", func(_ interface{}) {
+			aCalled = true
+			aCalledTimes += 1
+		})
+
+		u.Emit("a", nil)
+		destroyA()
+		u.Emit("a", nil)
+
+		if aCalled != expectedACalled {
+			t.Errorf("Expected a to have been called, but it wasn't. %t != %t", aCalled, expectedACalled)
+		}
+
+		if aCalledTimes != expectedACalledTimes {
+			t.Errorf("Expected a to have been called %d times, but it was called %d.", expectedACalledTimes, aCalledTimes)
+		}
+	})
+
+	t.Run("multiple_off", func(t *testing.T) {
+		u := NewBus()
+
+		aCalled := false
+		aCalledTimes := 0
+		expectedACalled := true
+		expectedACalledTimes := 1
+
+		bCalled := false
+		bCalledTimes := 0
+		expectedBCalled := true
+		expectedBCalledTimes := 1
+
+		cCalled := false
+		cCalledTimes := 0
+		expectedCCalled := true
+		expectedCCalledTimes := 1
+
+		destroyA := u.On("a", func(_ interface{}) {
+			aCalled = true
+			aCalledTimes += 1
+		})
+
+		destroyB := u.On("b", func(_ interface{}) {
+			bCalled = true
+			bCalledTimes += 1
+		})
+
+		destroyC := u.On("c", func(_ interface{}) {
+			cCalled = true
+			cCalledTimes += 1
+		})
+
+		u.Emit("a", nil)
+		destroyA()
+		u.Emit("a", nil)
+		u.Emit("a", nil)
+
+		u.Emit("b", nil)
+		destroyB()
+		u.Emit("b", nil)
+		u.Emit("b", nil)
+
+		u.Emit("c", nil)
+		destroyC()
 
 		if aCalled != expectedACalled {
 			t.Errorf("Expected a to have been called, but it wasn't. %t != %t", aCalled, expectedACalled)
